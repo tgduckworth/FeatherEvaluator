3b2c3a32ea319f5ba2ca415debe69ef7a4b58cf8
tgduckworth <tgduckworth@gmail.com>
2016-12-02 20:30:19 -0600

    First attempt at the field portion of the proof. It's unclear what should be done for variables since no rule directly applies to them. This problem manifests itself as a subcase of the field case as well.

--- FEV_Properties.v
+++ FEV_Properties.v
@@ -1,3 +1,4 @@
+Require Import Metatheory.
 Require Import FJ_Definitions.
 
 Theorem feval_correct:
@@ -5,7 +6,18 @@ Theorem feval_correct:
 Proof.
   intro. induction e1; intros.
   - simpl in H. injection H. intros. rewrite H0. (* apply eval_context. *) admit.
-  - 
+  - destruct e1 eqn:D.
+    + inversion H. rewrite H1. admit.
+    + admit.
+    + admit.
+    + simpl in IHe1. remember (get c ct) as ofs. destruct ofs.
+      * remember (snd (fst p)) as fs. remember (combine (List.map fst fs) l) as fes.
+        apply eval_field with (fs:=fs) (fes:=fes).
+        --  
+        --
+        --
+      * simpl in H. rewrite <- Heqofs in H. discriminate.
+ (*apply eval_field with (fs:=snd (get c ct)) (fes:=combine (List.map fst fs) l).*)
   -
   -
 Admitted.
