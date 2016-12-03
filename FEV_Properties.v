1d40d55af2541903e6a336922eb869a4a04359fc
shamila wickramasuriya <shamila wickramasuriya>
2016-12-03 14:21:40 -0600

    Adding an optional theorem in FEV_Properties

--- FEV_Properties.v
+++ FEV_Properties.v
@@ -1,6 +1,15 @@
 Require Import Metatheory.
 Require Import FJ_Definitions.
 
+(*How about this theorem????*)
+Theorem feval_eq : forall (e1 e2 : exp), eval e1 e2 = True <-> feval e1 = feval e2.
+Proof.
+  split.
+  - intros. admit.
+  - intros. admit.
+Admitted.
+
+
 Theorem feval_correct:
   forall (e1 e2:exp) (ct:ctable), feval e1 ct = Some e2 -> eval e1 e2.
 Proof.
@@ -13,11 +22,11 @@ Proof.
     + simpl in IHe1. remember (get c ct) as ofs. destruct ofs.
       * remember (snd (fst p)) as fs. remember (combine (List.map fst fs) l) as fes.
         apply eval_field with (fs:=fs) (fes:=fes).
-        --  
-        --
-        --
+        -- admit.
+        -- admit.
+        -- admit.
       * simpl in H. rewrite <- Heqofs in H. discriminate.
  (*apply eval_field with (fs:=snd (get c ct)) (fes:=combine (List.map fst fs) l).*)
-  -
-  -
+  - admit.
+  - admit.
 Admitted.
