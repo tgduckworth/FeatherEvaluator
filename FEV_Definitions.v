cacaf6c9bd8e8d5ea2c0367d665cebece1072e56
tgduckworth <tgduckworth@gmail.com>
2016-12-14 15:06:07 -0600

    Never mind the last commit; I didn't take the time to check for where things were going wrong. Shamila determined it was coming from an older part and rewriting it fixed the problem.

--- FEV_Definitions.v
+++ FEV_Definitions.v
@@ -80,127 +80,27 @@ Fixpoint feval (e:fexp) (fct:fctable) : option fexp :=
         end
       end
     end
-  | e =>
-    match decomp e with
-    | (e, nil) =>
-      match e with
-      | f_field e fn =>
-        match decomp e with
-        | (e1, nil) =>
-          match feval e1 fct with
-          | Some e1' => Some (f_field e1' fn) (* RC-FIELD *)
-          | None => None (* Unable to to step subexpression of field access *)
-          end
-        | (e1, ps) =>
-          match e1 with
-          | f_new cn =>
-            match get cn fct with
-            | Some (_, fs, _) => get fn (combine (List.map fst fs) ps) (* R-FIELD *)
-            | None => None (* Class not found *)
-            end
-          | e1 => None (* TODO?*)
-          end
-        end
-      | f_meth e mn => None (* TODO *)
-      | f_new cn => None (* TODO *)
-      | f_var v => None (* TODO *)
-      | f_apply e1 e2 => None (* Should not happen by definition of decomp *)
-      end
-    | (e, ps) => None (* Shouldn't happen because of the apply case above *)
-    end
-  end.
-
-Fixpoint feval (e:fexp) (ct:ctable) : option fexp :=
-  .
-
-Fixpoint feval (e:fexp) (ct:ctable) : option fexp :=
-  match e with
   | f_field e fn =>
-    match decomp e with
-    | (e, nil) => (* Field access on non-apply *)
-      match feval e ct with
-      | Some e' => Some (f_field e' fn) (* RC-FIELD *)
-      | None => None (* Field access on a non-apply that doesn't simplify *)
-      end
-    | (eb, fes) => (* Field access on apply *)
-      match eb with
-      | f_new cn => (* Field access on an instantiation *)
-        match (get cn ct) with
-        | Some (_, fs, _) => get fn (combine (List.map fst fs) fes) (* R-FIELD *)
-        | None => None (* Class not found *)
-        end
-      | eb =>                 (* On anything else we just try to step the *)
-        match feval e ct with (* field subexpression, as above *)
-        | Some e' => Some (f_field e' fn) (* RC-FIELD *)
-        | None => None (* The expression being accessed doesn't step *)
-        end
-      end
-    end
-  | f_apply e1 e2 =>
-    match feval e2 ct with
-    | Some e2' => Some (f_apply e1 e2')
-    | None =>
-      match feval e1 ct with
-      | Some e1' => Some (f_apply e1' e2)
-      | None =>
-        match decomp e1 with
-        | (eb, fes) =>
-          match feval eb ct with
-          | Some 
-          |
+    match e with
+    | f_apply e1 e2 =>
+      let (eb, ps) := decomp (f_apply e1 e2) in
+        match eb with
+        | f_new cn =>
+          match get cn fct with
+          | Some (_, fs, _) => get fn (combine (List.map fst fs) ps) (* R-FIELD *)
+          | None => None (* Class not found *)
           end
-        | (e1, nil) => None
+        | eb => None (* TODO?*)
         end
+    | e =>
+      match feval e fct with
+      | Some e1' => Some (f_field e1' fn) (* RC-FIELD *)
+      | None => None (* Unable to to step subexpression of field access *)
       end
     end
-  | f_var _ => None
-  | f_meth e mn =>
-    match feval e ct with
-    | Some e' => Some (f_meth e' mn)
-    | None => None
-    end
-  | f_new cn => None
-  end.
-
-Fixpoint feval (e:fexp) (ct:ctable) : option fexp :=
-  match e with
-  | f_field (f_apply (f_new C) al) f =>
-    match (get C ct) with
-    | Some (_, fs, _) =>
-      match fexp2list al with
-      | Some es => get f (combine (List.map fst fs) es) (* R-FIELD *)
-      | None => None (* Arguments could not be made into a list *)
-    | None => None (* Class not found *)
-    end
-  | f_apply (f_meth (f_new C) m) al =>
-    match (get C ct) with
-    | Some (_, _, ms) =>
-      match (get m ms) with
-      | Some (_,en,ex) => Some (subst_exp ((this,(e_new C al))::(combine (List.map fst en) al)) ex) (* R-INVK *)
-      | None => None (* Insert case for inheritance here *)
-      end
-    | None => None (* Class not found *)
-    end
-  | f_field e f =>
-    match (feval e ct) with
-    | Some e' => Some (f_field e' f) (* RC-FIELD *)
-    | None => None (* Field access on an expression that isn't an object *)
-    end            (* but cannot be reduced produces nothing *)
-  | f_apply (f_meth e m) al =>
-    match (feval e ct) with
-    | Some e' => Some (f_apply (f_meth e' m) al) (* RC-INVK-RECV *)
-    | None =>
-      match feval al ct with
-      | Some al' => Some (f_apply (f_meth e m) al') (* RC-INVK-ARG *)
-      | None => None (* Method invocation on an expression that isn't an *)
-      end            (* object but cannot be reduced produces nothing *)
-    end
-  | f_apply (f_new C) al =>
-    match feval al ct with
-    | Some al' => Some (f_apply (f_new C) al') (* RC-NEW-ARG *)
-    | None => None (* If an object's argument expressions do not reduce, then *)
-    end            (* the expression as a whole does not reduce *)
-  | f_var _ => None (* Catch-all for variables, which do not step to anything *)
+  | f_meth e mn => None (* TODO *)
+  | f_new cn => None (* TODO *)
+  | f_var v => None (* TODO *)
   end.
 
 Fixpoint teval (e:exp) (ct:ctable) (n:nat) : exp :=
