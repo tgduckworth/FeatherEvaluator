20642241f787cb6aedc781e92c0d8d53df0e7d01
tgduckworth <tgduckworth@gmail.com>
2016-12-15 13:58:09 -0600

    Proof case for RC-INVK-RECV completed.

--- FEV_Properties.v
+++ FEV_Properties.v
@@ -3,7 +3,7 @@ Require Import FJ_Definitions.
 Require Import FEV_Definitions.
 
 Theorem feval_sound:
-  forall (e1 e2:fexp) (ct:fctable), feval e1 ct = Some e2 -> eval (fexp2exp e1) (fexp2exp e2).
+  forall (e1 e2:fexp) (fct:fctable), feval e1 fct = Some e2 -> eval (fexp2exp e1) (fexp2exp e2).
 Proof.
   intro. induction e1; intros.
   - simpl in H. discriminate.
@@ -13,8 +13,12 @@ Proof.
     + admit.
     + simpl in H. discriminate.
     + admit.
-  - simpl in *. destruct (feval e1 ct).
-    + injection H. intro. rewrite <- H0. simpl.
+  - simpl in *. destruct (feval e1 fct) eqn:D.
+    + apply IHe1 in D. remember (fun (e:exp) => e_meth e m nil) as EE'.
+      apply eval_context with (EE:=EE') in D.
+      * rewrite HeqEE' in D. injection H. intro. rewrite <- H0. simpl.
+        assumption.
+      * rewrite HeqEE'. apply ec_meth_arg0.
     + discriminate.
   - simpl in H. discriminate.
   - admit.
